d302bfff6a822d33120ec82ec08bbdcc45abb48f
Fabian Hueske <fhueske@apache.org>
2020-04-20 12:02:45 +0200

    Load data into MySQL not into Minio

--- flinksql-init/create_tables.sql
+++ flinksql-init/create_tables.sql
@@ -1,16 +1,5 @@
 USE CATALOG hcat;
 
-CREATE TABLE dev_region (
-  r_regionkey     INTEGER,
-  r_name          STRING,
-  r_comment       STRING
-) WITH (
-  'connector.type' = 'filesystem',
-  'connector.path' = 's3://sql-demo/region.tbl',
-  'format.type' = 'csv',
-  'format.field-delimiter' = '|'
-);
-
 CREATE TABLE prod_region (
   r_regionkey     INTEGER,
   r_name          STRING,
@@ -24,18 +13,6 @@ CREATE TABLE prod_region (
   'connector.password' = 'demo-sql',
   'connector.lookup.cache.max-rows' = '1',
   'connector.lookup.cache.ttl' = '0s'
-);
-
-CREATE TABLE dev_nation (
-  n_nationkey     INTEGER,
-  n_name          STRING,
-  n_regionkey     INTEGER,
-  n_comment       STRING
-) WITH (
-  'connector.type' = 'filesystem',
-  'connector.path' = 's3://sql-demo/nation.tbl',
-  'format.type' = 'csv',
-  'format.field-delimiter' = '|'
 );
 
 CREATE TABLE prod_nation (
@@ -52,22 +29,6 @@ CREATE TABLE prod_nation (
   'connector.password' = 'demo-sql',
   'connector.lookup.cache.max-rows' = '1',
   'connector.lookup.cache.ttl' = '0s'
-);
-
-CREATE TABLE dev_customer (
-  c_custkey       INTEGER,
-  c_name          STRING,
-  c_address       STRING,
-  c_nationkey     INTEGER,
-  c_phone         STRING,
-  c_acctbal       DOUBLE,
-  c_mktsegment    STRING,
-  c_comment       STRING
-) WITH (
-  'connector.type' = 'filesystem',
-  'connector.path' = 's3://sql-demo/customer.tbl',
-  'format.type' = 'csv',
-  'format.field-delimiter' = '|'
 );
 
 CREATE TABLE prod_customer (
@@ -90,17 +51,6 @@ CREATE TABLE prod_customer (
   'connector.lookup.cache.ttl' = '0s'
 );
 
-CREATE TABLE dev_rates (
-  rs_timestamp    TIMESTAMP(3),
-  rs_symbol       STRING,
-  rs_rate         DOUBLE
-) WITH (
-  'connector.type' = 'filesystem',
-  'connector.path' = 's3://sql-demo/rates.tbl',
-  'format.type' = 'csv',
-  'format.field-delimiter' = '|'
-);
-
 CREATE TABLE prod_rates (
   rs_timestamp    TIMESTAMP(3),
   rs_symbol       STRING,
@@ -116,45 +66,4 @@ CREATE TABLE prod_rates (
   'connector.lookup.cache.ttl' = '0s'
 );
 
-CREATE TABLE dev_orders (
-  o_orderkey      INTEGER,
-  o_custkey       INTEGER,
-  o_orderstatus   STRING,
-  o_totalprice    DOUBLE,
-  o_currency      STRING,
-  o_ordertime     TIMESTAMP(3),
-  o_orderpriority STRING,
-  o_clerk         STRING, 
-  o_shippriority  INTEGER,
-  o_comment       STRING,
-  WATERMARK FOR o_ordertime AS o_ordertime - INTERVAL '5' MINUTE
-) WITH (
-  'connector.type' = 'filesystem',
-  'connector.path' = 's3://sql-demo/orders.tbl',
-  'format.type' = 'csv',
-  'format.field-delimiter' = '|'
-);
-
-CREATE TABLE dev_lineitem (
-  l_orderkey       INTEGER,
-  l_partkey        INTEGER,
-  l_suppkey        INTEGER,
-  l_linenumber     INTEGER,
-  l_quantity       DOUBLE,
-  l_extendedprice  DOUBLE,
-  l_discount       DOUBLE,
-  l_tax            DOUBLE,
-  l_currency       STRING,
-  l_returnflag     STRING,
-  l_linestatus     STRING,
-  l_ordertime      TIMESTAMP(3),
-  l_shipinstruct   STRING,
-  l_shipmode       STRING,
-  l_comment        STRING
-) WITH (
-  'connector.type' = 'filesystem',
-  'connector.path' = 's3://sql-demo/lineitem.tbl',
-  'format.type' = 'csv',
-  'format.field-delimiter' = '|'
-);
-
+QUIT;
