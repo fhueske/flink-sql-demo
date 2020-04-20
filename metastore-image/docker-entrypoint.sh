1f749bc7ffcb9490bfefb005c5706bb9c6fae47c
Fabian Hueske <fhueske@apache.org>
2020-04-20 07:12:52 +0200

    Start Metastore with empty catalog

--- metastore-image/docker-entrypoint.sh
+++ metastore-image/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 # start Derby
 /opt/db-derby-10.10.2.0-bin/bin/startNetworkServer -h 0.0.0.0 &
 
-# create Metastore schema. Not needed because we restored a database backup
-# /opt/apache-hive-2.3.6-bin/bin/schematool -initSchema -dbType derby
+# create Metastore schema
+/opt/apache-hive-2.3.6-bin/bin/schematool -initSchema -dbType derby
 
 # start Metastore
 /opt/apache-hive-2.3.6-bin/bin/hive --service metastore -p 9083
