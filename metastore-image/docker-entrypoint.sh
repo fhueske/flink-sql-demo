3f7c9d8383a99931c65685abf4cf69a485a5df7d
Fabian Hueske <fhueske@apache.org>
2020-09-09 11:29:13 +0200

    Update to Hive 3.1.2

--- metastore-image/docker-entrypoint.sh
+++ metastore-image/docker-entrypoint.sh
@@ -4,7 +4,7 @@
 /opt/db-derby-10.10.2.0-bin/bin/startNetworkServer -h 0.0.0.0 &
 
 # create Metastore schema
-/opt/apache-hive-2.3.6-bin/bin/schematool -initSchema -dbType derby
+/opt/apache-hive-3.1.2-bin/bin/schematool -initSchema -dbType derby
 
 # start Metastore
-/opt/apache-hive-2.3.6-bin/bin/hive --service metastore -p 9083
+/opt/apache-hive-3.1.2-bin/bin/hive --service metastore -p 9083
